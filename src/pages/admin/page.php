35960559d7f066f8a6897960b22a8cf7ae0aeb59
Nguyễn Văn Thy <coder.haibach@gmail.com>
2021-04-06 23:09:10 +0700

    Thêm route-link vào layout

--- src/pages/admin/page.php
+++ src/pages/admin/page.php
@@ -1,18 +1,20 @@
 <section id="rubydurian-app" class="">
   <!-- Navigation -->
   <nav>
-    <div class="du-nav__inner">
+    <div class="du-nav__top">
       <a class="du-nav__logo">
         Logo
       </a>
       <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
+        <li><router-link to="/admin.php?page=rubydurian">Dashboard</router-link></li>
+        <li><router-link to="/admin.php?page=rubydurian-calendar">Calendar</router-link></li>
+        <li><router-link to="/admin.php?page=rubydurian-customers">Customers</router-link></li>
+        <li><router-link to="/admin.php?page=rubydurian-staffs">Staffs</router-link></li>
+        <li><router-link to="/admin.php?page=rubydurian-services">Services</router-link></li>
       </ul>
+    </div>
+    <div class="du-nav__bottom">
+      <a href="#">Options</a>
     </div>
   </nav>
 
