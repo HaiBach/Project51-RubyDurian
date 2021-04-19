07ed4bd473224b9ac0efe07395452758a15bd29c
Nguyễn Văn Thy <coder.haibach@gmail.com>
2021-04-19 21:54:33 +0700

    Tạo component Staff Items

--- src/components/iframe/Iframe-TabStaff.vue
+++ src/components/iframe/Iframe-TabStaff.vue
@@ -1,50 +1,41 @@
 <template>
-  <h1>STAFF</h1>
   <div class="tab tab__time">
     <div class="tab__inner du-flex du-flex-col du-px-10 du-pt-5">
-      <div class="pick-date du-flex du-justify-center">
-        <div class="du-p-2.5 du-rounded-md">Today &nbsp; &nbsp; 07 Dec - 13 Dec &nbsp;</div>
+
+      <div class="staff-group du-pt-5 du-pb-10">
+        <ServiceItem type="item"></ServiceItem>
+        <StaffItems></StaffItems>
       </div>
+
+      <div class="staff-group du-pt-5 du-pb-10">
+        <ServiceItem type="item"></ServiceItem>
+        <StaffItems></StaffItems>
+      </div>
+
     </div>
+
+    <!-- Footer -->
+    <Footer to="/sign-in"></Footer>
   </div>
 </template>
 
 
 <script>
+import Footer from './Iframe-Footer.vue'
+import ServiceItem from './TabService-Item.vue'
+import StaffItems from './TabStaff-Items.vue'
+
 export default {
   data() {
     return {
     }
-  }
+  },
+  components: {
+    Footer, ServiceItem, StaffItems,
+  },
 }
 </script>
 
 
 <style scoped>
-  /** Calendar */
-  .calendar {
-    width: 600px;
-    margin-left: auto;
-    margin-right: auto;
-  }
-  .calendar__header li {
-    width: 14.28%;
-    color: #4B5563;
-  }
-  .calendar__week li {
-    width: 14.28%;
-    padding: 16px;
-    border-right: 1px solid #6B7280;
-    font-weight: 500;
-    text-align: center;
-  }
-  .calendar__week li:last-child {
-    border-right-width: 0;
-    color: #9CA3AF;
-    font-weight: 400;
-  }
-  .calendar__week li.du-inactive {
-    color: #9CA3AF;
-    font-weight: 400;
-  }
 </style>
---
+++ src/components/iframe/TabStaff-Items.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="staff-items">
+    <ul>
+      <li class="staff-item">
+        <div class="item__inner du-flex du-items-center du-px-5 du-py-2.5 du-border du-border-dashed du-border-gray-400 du-rounded-lg du-bg-white">
+          <div class="item__left">
+            <div class="item__avatar du-w-12 du-h-12 du-mr-5 du-rounded-full du-bg-gray-200 do-text-gray-500">
+              <i class="du-icon-people"></i>
+            </div>
+          </div>
+          <div class="item__right">
+            <div class="item__name du-text-gray-500 du-font-bold">Any Staff</div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+</style>
