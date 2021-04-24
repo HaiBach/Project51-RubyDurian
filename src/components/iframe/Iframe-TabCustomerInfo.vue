6e5b4ddc24c605db9120aced6cb1efda38e191ac
Nguyễn Văn Thy <coder.haibach@gmail.com>
2021-04-25 01:44:33 +0700

    Tạo component Customer Info

--- src/components/iframe/Iframe-TabCustomerInfo.vue
+++ src/components/iframe/Iframe-TabCustomerInfo.vue
@@ -1,50 +1,113 @@
 <template>
-  <h1>CUSTOMER INFO</h1>
-  <div class="tab tab__time">
+  <div class="tab tab__customer">
     <div class="tab__inner du-flex du-flex-col du-px-10 du-pt-5">
-      <div class="pick-date du-flex du-justify-center">
-        <div class="du-p-2.5 du-rounded-md">Today &nbsp; &nbsp; 07 Dec - 13 Dec &nbsp;</div>
+
+      <!-- Tab Header -->
+      <div class="tab__header">
+        <div class="tab__name du-my-4 du-text-center">
+          <h4>Hi Nguyen Van A</h4>
+          <p class="du-text-gray-400">Please review your infomation</p>
+        </div>
       </div>
+
+      <!-- Tab Content -->
+      <div class="tab__content du-mt-4 du-mb-8">
+        <div class="tab__txt">
+          <h2>Nguyen Van A</h2>
+          <p>vana.nguyen@gmail.com<br>
+            +1 (808)-216-5195<br>
+            25  Emma Street, Hawaii State, Moanalua City - 96819
+          </p>
+        </div>
+        <div class="tab__btn du-mt-3">
+          <a href="#" class="btn-update-profile du-inline-block du-px-5 du-py-2 du-rounded-md du-bg-gray-100 du-text-gray-600 hover:du-bg-gray-700 hover:du-text-white">
+            <i class="icon-pencil"></i><span class="btn__text">Update Profile</span>
+          </a>
+        </div>
+      </div>
+
+      <!-- Tab Footer -->
+      <div class="tab__footer du-pt-8 du-mb-8 du-border-t du-border-gray-300 du-text-gray-500 du-text-center">
+        <p class="du-text-sm">No cancellation or rescheduling is permitted within 24 hour(s) of the appointment time</p>
+        <div class="form du-flex du-justify-center du-items-center du-mt-2.5">
+          <div class="input-policy">
+            <input type="checkbox" id="agree-private-policy" class="du-absolute du-invisible">
+            <label for="agree-private-policy" class="du-relative du-block du-text-sm">I agree to the <u>cancellation policy</u></label>
+          </div>
+        </div>
+      </div>
+
     </div>
+
+    <Footer to="/summary"></Footer>
   </div>
 </template>
 
 
 <script>
+import Footer from './Iframe-Footer.vue'
+
 export default {
   data() {
     return {
     }
+  },
+  components: {
+    Footer,
   }
 }
 </script>
 
 
 <style scoped>
-  /** Calendar */
-  .calendar {
-    width: 600px;
+  .tab__inner {
+    width: 680px;
     margin-left: auto;
     margin-right: auto;
   }
-  .calendar__header li {
-    width: 14.28%;
-    color: #4B5563;
+  .tab__txt h2 {
+    margin-bottom: 8px;
+    font-size: 20px;
+    font-weight: 700;
   }
-  .calendar__week li {
-    width: 14.28%;
-    padding: 16px;
-    border-right: 1px solid #6B7280;
-    font-weight: 500;
-    text-align: center;
+  .tab__txt p {
+    color: #6B7280;
   }
-  .calendar__week li:last-child {
-    border-right-width: 0;
-    color: #9CA3AF;
-    font-weight: 400;
+
+  /** Button Update Profile */
+  .btn-update-profile {
+    font-size: 14px;
+    line-height: 1.6;
   }
-  .calendar__week li.du-inactive {
-    color: #9CA3AF;
-    font-weight: 400;
+
+  /** Form */
+  label {
+    padding-left: 35px;
+    line-height: 22px;
+    cursor: pointer;
+  }
+  label::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 24px;
+    height: 24px;
+    border-radius: 6px;
+    border: 2px solid #6B7280;
+    background-color: #fff;
+    transition: background-color 0.2s;
+  }
+  label::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+  }
+  input[type="checkbox"]:checked + label::before {
+    border-color: #F59E0B;
+    /* background-color: #FEF3C7; */
   }
 </style>
--- src/components/iframe/Iframe-TabSignIn.vue
+++ src/components/iframe/Iframe-TabSignIn.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="tab tab__time">
-    <div class="tab__inner du-px-5 du-pt-5">
+    <div class="tab__inner du-flex du-flex-col du-justify-center du-items-center du-p-5">
       <div class="signin-form">
+
         <form action="">
           <div class="signin__email du-mb-2.5">
             <input type="email" name="email" value="" placeholder="Email">
@@ -9,14 +10,26 @@
           <div class="signin__password du-mb-2.5">
             <input type="password" name="password" value="" placeholder="Password">
           </div>
+          
+          <!-- SIGN-IN SUBMIT -->
           <div class="signin__submit du-mb-2.5">
-            <button type="submit" class="du-px-5 du-py-2.5 du-rounded-md du-bg-yellow-400 du-text-gray-800 du-text-center">Sign In</button>
+            <router-link to="/customer-info" class="signin__submit_link du-block du-px-5 du-py-2.5 du-rounded-lg du-bg-yellow-400 du-text-gray-800 du-text-sm du-text-center">Sign In</router-link>
+            <!-- <button type="submit" class="du-px-5 du-py-2.5 du-rounded-lg du-bg-yellow-400 du-text-gray-800 du-text-sm du-text-center">Sign In</button> -->
           </div>
+          <!-- .signin__submit -->
+
         </form>
+        <div class="signin-form__links du-flex du-justify-center du-items-center">
+          <a href="#" id="forgot-password" class="signin-form__link du-px-4 du-my-4 du-border-r du-border-gray-300 du-text-gray-400 du-text-sm du-leading-4 hover:du-text-gray-800">Forgot Password?</a>
+          <a href="#" id="new-profile" class="signin-form__link du-px-4 du-my-4 du-text-gray-400 du-text-sm du-leading-4 hover:du-text-gray-800">Create New Profile</a>
+        </div>
       </div>
       
-      <div class="signin-social">
-        
+      <div class="signin-social du-pt-8 du-mt-4 du-border-t du-border-gray-300">
+        <button class="du-block du-px-5 du-py-2.5 du-mb-2.5 du-rounded-lg du-bg-gray-100 du-text-gray-600 du-text-sm du-text-center">Sign in with Facebook</button>
+        <button class="du-block du-px-5 du-py-2.5 du-mb-2.5 du-rounded-lg du-bg-gray-100 du-text-gray-600 du-text-sm du-text-center">Sign in with Google</button>
+        <!-- <a href="#" id="new-profile" class="signin-form__link du-block du-px-4 du-py-4 du-text-gray-400 du-text-sm du-leading-4 du-text-center hover:du-text-gray-800">Skip login process →</a> -->
+        <router-link to="/customer-info" class="signin-form__link du-block du-px-4 du-py-4 du-text-gray-400 du-text-sm du-leading-4 du-text-center hover:du-text-gray-800">Skip login process →</router-link>
       </div>
     </div>
   </div>
@@ -34,15 +47,37 @@ export default {
 
 
 <style scoped>
+  form {
+    width: 320px;
+  }
   input[type="email"],
   input[type="password"] {
-    width: 320px;
-    padding: 8px 18px;
-    border-radius: 6px;
+    width: 100%;
+    padding: 10px 18px;
+    border-radius: 8px;
     border: 1px solid #D1D5DB;
     background-color: #fff;
+    font-size: 14px;
   }
   button {
+    width: 100%;
+  }
+  .signin__submit_link {
+    line-height: 1.6;
+  }
+  .tab__inner {
+    min-height: calc(100vh - 48px);
+  }
+
+  /** SIGN-IN FORM */
+  .signin-form {
+    /* max-width: 320px;
+    margin-left: auto;
+    margin-right: auto; */
+  }
+
+  /** SIGN-IN SOCIAL */
+  .signin-social {
     width: 320px;
   }
 </style>
